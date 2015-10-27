343d1f6efbc303d757be8e5faf982e2ed949e161
Claire Annan <claire.annan.me@gmail.com>
2015-10-27 12:35:50 -0400

    Add active class to current detail.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@
     cache: false,
     url: "https://api.instagram.com/v1/users/336431628/media/recent/?access_token=829290775.5f79be6.e09d3652a36f42cea5f8426426d844f8",
     success: function(data) {
-      console.log('ig loading...');
       var d = data.data;
       buildList(d);
       for(var i=0; i<d.length; i++){
@@ -25,7 +24,7 @@
         };
         
         addThumb(d[i].images.thumbnail);
-        addMarker(latLong);
+        addMarker(i, latLong);
         addDetails(imageInfo);      
       }
     }
@@ -37,11 +36,15 @@
     list.innerHTML = '';
     for(var i = 0; i < data.length; i++){
       var option = document.createElement('option');
+      var latLong = {
+        name: data[i].location.name, 
+        lat: data[i].location.latitude, 
+        lng: data[i].location.longitude
+      };
+
       option.text = data[i].location.name;
       option.value = i;
       list.appendChild(option);
-
-      var latLong = {name: data[i].location.name, lat: data[i].location.latitude, lng: data[i].location.longitude};
       $(list).on('change', latLong, centerLocation);
       /* See http://api.jquery.com/on/#passing-data */
     }
@@ -73,30 +76,33 @@
     //console.log('thumb: '+ image);
   }
 
-  function addMarker(location){
+  function addMarker(index, location){
     var marker = new google.maps.Marker({
       map: map,
       position: location,
       title: 'Click for image',
+      index: index,
     });
+
+    var details = $('.details .image-wrapper');
 
     marker.addListener('click', function() {
       map.setZoom(10);
       map.setCenter(marker.getPosition());
+      $(details[marker.index]).addClass('active');
       $('#map-canvas').animate({'height': 300});
-      $('.details').fadeIn('slow');   
+      $('.details').fadeIn('slow');
+
     });
   }
 
   function initMap() {
   	var start = {lat: 31.9639977, lng: 35.90654797};
-
     map = new google.maps.Map(document.getElementById('map-canvas'), {
       center: start,
       scrollwheel: false,
       zoom: 3,
     });
-    console.log('map initialized...');
   }
 
   $(document).ready(function(){
